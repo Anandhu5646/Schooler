b911aa5e11ab8b282164240295bf48f08b2d50b4
Anandhu5646 <anandhus05646@gmail.com>
2023-07-17 20:28:05 +0530

    dashboard design updated

--- client/src/components/admin/dashboard/DashBoard.jsx
+++ client/src/components/admin/dashboard/DashBoard.jsx
@@ -1,105 +1,50 @@
 import React from "react";
-import { Col, Card, Row } from "react-bootstrap";
-import "./DashBoard.css";
+import { CCardGroup, CCard, CCardBody, CCardImage, CCardTitle, CCardText } from "@coreui/react";
+import reactImage from "/images/react.jpg";
 
-const DashBoard = ({ orderCount }) => {
-
-
-
-
-
-  
+const Dashboard = ({ totalStudents, totalClubs, totalFaculty }) => {
   return (
-    <div style={{ width: "80%", marginLeft: "200px", marginTop: "50px" }}>
-      <Row className="d-flex justify-content-between" >
-        <Col md={6} lg={4}>
-          <Card className="mb-4 card">
-            <article className="icontext">
-              <span className="icon icon-sm rounded-circle bg-success-light">
-                <i className="text-success material-icons md-local_shipping"></i>
-              </span>
-              <div className="text">
-                <h4 className="mb-1 card-title">
-                  <img
-                    style={{
-                      width: "100px",
-                      height: "80px",
-                      marginRight: "20px",
-                      marginTop:"10px",
-                    }}
-                    className="img-fluid"
-                    src="https://static.vecteezy.com/system/resources/previews/000/511/962/original/vector-student-glyph-black-icon.jpg"
-                    alt=""
-                  />
-                 Total Students
-                </h4>
-                <span style={{ marginLeft: "115px" }}>
-                  <strong>{orderCount}10</strong> 
-                </span>
-              </div>
-            </article>
-          </Card>
-        </Col>
-        <Col md={6} lg={4}>
-          <Card className="mb-4 card">
-            <article className="icontext">
-              <span className="icon icon-sm rounded-circle bg-success-light">
-                <i className="text-success material-icons md-local_shipping"></i>
-              </span>
-              <div className="text">
-                <h4 className="mb-1 card-title">
-                  <img
-                    style={{
-                      width: "100px",
-                      height: "80px",
-                      marginRight: "20px",
-                      
-                      marginTop:"10px"
-                    }}
-                    className="img-fluid"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmiTP4daKejpHCtaOktcnWofHGcfyBMihctPgNeDUveh_aA90tmQR4fw6HnagvKL6iBng&usqp=CAU"
-                    alt=""
-                  />
-                 Total Faculties
-                </h4>
-                <span style={{ marginLeft: "115px" }}>
-                  <strong>{orderCount}</strong> 10
-                </span>
-              </div>
-            </article>
-          </Card>
-        </Col>
-        <Col md={6} lg={4}>
-          <Card className="mb-4 card">
-            <article className="icontext">
-              <span className="icon icon-sm rounded-circle bg-success-light">
-                <i className="text-success material-icons md-local_shipping"></i>
-              </span>
-              <div className="text">
-                <h4 className="mb-1 card-title">
-                  <img
-                    style={{
-                      width: "100px",
-                      height: "80px",
-                      marginRight: "20px",
-                      marginTop:"10px"
-                    }}
-                    className="img-fluid"
-                    src="https://cdn-icons-png.flaticon.com/128/6535/6535457.png"
-                    alt=""
-                  />
-                 Total Clubs
-                </h4>
-                <span style={{ marginLeft: "115px" }}>
-                  <strong>{orderCount}</strong> 10
-                </span>
-              </div>
-            </article>
-          </Card>
-        </Col>
-      </Row>
-    </div>
+    <CCardGroup>
+      <CCard>
+        <CCardImage orientation="top" src={reactImage} />
+        <CCardBody>
+          <CCardTitle>Total Students</CCardTitle>
+          <CCardText>
+            This is a wider card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.
+          </CCardText>
+          <CCardText>
+            <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+          </CCardText>
+        </CCardBody>
+      </CCard>
+      <CCard>
+        <CCardImage orientation="top" src={reactImage} />
+        <CCardBody>
+          <CCardTitle>Total Clubs</CCardTitle>
+          <CCardText>
+            This card has supporting text below as a natural lead-in to additional content.
+          </CCardText>
+          <CCardText>
+            <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+          </CCardText>
+        </CCardBody>
+      </CCard>
+      <CCard>
+        <CCardImage orientation="top" src={reactImage} />
+        <CCardBody>
+          <CCardTitle>Total Faculty</CCardTitle>
+          <CCardText>
+            This is a wider card with supporting text below as a natural lead-in to additional content.
+            This card has even longer content than the first to show that equal height action.
+          </CCardText>
+          <CCardText>
+            <small className="text-medium-emphasis">Last updated 3 mins ago</small>
+          </CCardText>
+        </CCardBody>
+      </CCard>
+    </CCardGroup>
   );
 };
 
-export default DashBoard;
+export default Dashboard;
