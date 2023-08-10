63f864ba1580776ae575e498550888394190c8e5
Anandhu5646 <anandhus05646@gmail.com>
2023-08-10 15:24:25 +0530

    done

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.resolve() + "/public"));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://schoolerr.netlify.app"],
+    origin: ["http://localhost:3000","https://schoolerr.netlify.app","https://schooler.onrender.com"],
     credentials: true,
   })
 );
