ea9d8c2d2cf77e84670d7946527766b7e2501890
Anandhu5646 <anandhus05646@gmail.com>
2023-08-11 15:17:37 +0530

    url update

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,8 @@ import ErrPage from './pages/404page';
 
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:1800"
-  // axios.defaults.baseURL ="https://schooler.sigag.online"
+  // axios.defaults.baseURL = "http://localhost:1800"
+  axios.defaults.baseURL ="https://schooler.sigag.online"
   axios.defaults.withCredentials = true;
 
   const { loading } = useSelector((state) => state);
